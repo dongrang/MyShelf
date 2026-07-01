0f93c364f3a187a7c68afb1ef51c4757915cab5b
dongrang <andrewzeng1020@gmail.com>
2026-07-01 04:06:25 -0400

    cleanup + pages skeleton + db.json

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        GameVault: a backlog tracker for games I want to play, have played, or
+        dropped.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/FilteredList.tsx
@@ -0,0 +1,5 @@
+export default function FilteredList() {
+    return <div>
+        
+    </div>
+}
